2e3a7be548c5ddb9db242c9af663fe71cd26a7a5
kiruvidhya <kiru.srini@gmail.com>
2014-06-21 13:04:48 -0400

    Matrix and Solve
    Splitting Matrix and inverse Matrix

---
+++ cacheSolve.R
@@ -0,0 +1,21 @@
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+  ## If the inverse has already been calculated (and the matrix has not changed), 
+  ## then the cachesolve should retrieve the inverse from the cache
+cacheSolve <- function(x=matrix(), ...) {
+# Get the inverse matrix if it is cached  
+ s <- x$getinv()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  
+  # get the original matrix
+  data <- x$get()
+ 
+  # Convert Inverse Matrix
+  s <- solve(data, ...)
+  
+  # Set Inverse Matrix and set them into cache.
+  x$setinv(s)
+  s
+}
